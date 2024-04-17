57ae122f0376b11617a871692cc25e39ce2a1b86
blasckad <blasckad@gmail.com>
2024-04-17 22:42:38 +0300

    initial

---
+++ connection/connection.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser, FileType
+from configparser import ConfigParser
+from api import start_rest
+from producer import start_producer
+from multiprocessing import Queue
+
+if __name__ == "__main__":
+    # Parse the command line.
+    parser = ArgumentParser()
+    parser.add_argument('config_file', type=FileType('r'))
+    parser.add_argument('--reset', action='store_true')
+    args = parser.parse_args()
+    # Parse the configuration.
+    # See https://github.com/edenhill/librdkafka/blob/master/CONFIGURATION.md
+
+    config_parser = ConfigParser()
+    config_parser.read_file(args.config_file)
+    config = dict(config_parser['default'])
+    config.update(config_parser['data_input'])
+
+    requests_queue = Queue()
+
+    start_rest(requests_queue)    
+    start_producer(args, config, requests_queue)
